f4e9cf86f18120cffc6e909d9ca8a70d7e9dccbc
TheMinimalist <themnmlst.dev@protonmail.com>
2025-03-27 00:10:05 +0100

    Setting tickrate 60

--- main.py
+++ main.py
@@ -8,12 +8,16 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (0, 0, 0)
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color, rect=None, special_flags=0)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
