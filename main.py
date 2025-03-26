2ee78981e5ce72f2394d4da9874f3cb8a1f44b19
TheMinimalist <themnmlst.dev@protonmail.com>
2025-03-26 23:46:56 +0100

    rectangle on screen

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import pygame
+
 from constants import *
 
+
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    color = (255, 0, 0)
+    while True:
+        pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
