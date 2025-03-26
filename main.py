15a50a0fed65870fda5ab01e2c6ded4e3610a96d
TheMinimalist <themnmlst.dev@protonmail.com>
2025-03-27 00:03:58 +0100

    Filling the screen with black

--- main.py
+++ main.py
@@ -1,21 +1,19 @@
 import pygame
 
-from constants import *
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
 
 def main():
+    """ The main function """
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    color = (255, 0, 0)
+    color = (0, 0, 0)
     while True:
-        pygame.draw.rect(screen, color, pygame.Rect(30, 30, 60, 60))
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+        screen.fill(color, rect=None, special_flags=0)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
