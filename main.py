50b25c3b82ed6fed07d1443c2f931ce9f944db4f
TheMinimalist <themnmlst.dev@protonmail.com>
2025-03-27 23:39:38 +0100

    Added movement

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
+from player import Player
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-
 
 def main():
     """ The main function """
@@ -10,14 +10,19 @@ def main():
     color = (0, 0, 0)
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color, rect=None, special_flags=0)
-        pygame.display.flip()
+        player.draw(screen)
         dt = clock.tick(60) / 1000
+        player.update(dt)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
---
+++ player.py
@@ -0,0 +1,43 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
+
+class Player(CircleShape):
+    """ The players representation. A simple trianlge """
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the player class
+    def triangle(self):
+        """ The triangle """
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        color = (255, 255, 255)
+        line_width = 2
+        pygame.draw.polygon(screen, color, self.triangle(), line_width)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
